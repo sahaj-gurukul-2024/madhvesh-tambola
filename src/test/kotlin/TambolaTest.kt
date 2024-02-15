ce744d9a0fa1a74041c93c41cb65a58683947f07
Siddhant Jagdish Verma <siddhantjagdishv@Siddhants-MacBook-Pro.local>
2024-02-15 16:42:02 +0530

    Created Initial Test Case

--- src/test/kotlin/TambolaTest.kt
+++ src/test/kotlin/TambolaTest.kt
@@ -1,10 +1,29 @@
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class TambolaTest {
-
+    private lateinit var ticket: Ticket
+    private lateinit var game: Game
     @BeforeEach
     fun setup(){
-
+        game = Game()
     }
-    t
+    @Test
+    fun `check for Early 5 row claim as False in ticket`(){
+        //arrange
+        val ticketValues = mutableListOf(
+            mutableListOf(4, 16, 48, 63, 76),
+            mutableListOf(7, 23, 38, 52, 80),
+            mutableListOf(9, 25, 56, 64, 83)
+        )
+        val announcedNumbers = listOf(4, 23, 48, 63, 76, 90)
+        val claim = "Early 5"
+        //act
+        val ticket = Ticket(ticketValues)
+        val booleanValue = game.validate(ticket, announcedNumbers, claim)
+        //
+        assertFalse(booleanValue)
+    }
 }
