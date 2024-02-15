cdce40a2400a441bfc3d34e9559c625331072a4c
Siddhant Jagdish Verma <siddhantjagdishv@Siddhants-MacBook-Pro.local>
2024-02-15 17:12:55 +0530

    Revert "Refactor If-Else and Clean Code"
    This reverts commit b6b960a80fafa0157570678cc65d6732e4e4c229.

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,87 +1,107 @@
 class Game {
-    fun validate(ticket: Ticket, announcedNumbers: List<Int>, claim: String): Boolean {
-        when (claim) {
-            "Top Row" -> {
-                announcedNumbers.forEach() { announceValue ->
-                    var ticketMatrix = iterateThrough(ticket, announceValue)
-                    if (ticketMatrix[0].size == 0) {
-                        if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
-                            return true
-                    }
+    fun validate(ticket: Ticket, announcedNumbers: List<Int>, claim: String): Boolean
+    {
+        if(claim == "Top Row") {
+            announcedNumbers.forEach() { announceValue ->
+                var ticketMatrix = iterateThrough(ticket, announceValue)
+                if (ticketMatrix[0].size == 0) {
+                    if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
+                        return true
+                    else
+                        return false
                 }
-                return false
             }
-            "Bottom Row" -> {
-                announcedNumbers.forEach() { announceValue ->
-                    var ticketMatrix = iterateThrough(ticket, announceValue)
-                    if (ticketMatrix[ticketMatrix.size - 1].size == 0) {
-                        if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
-                            return true
-                    }
-                }
-                return false
-            }
-            "Middle Row" -> {
-                announcedNumbers.forEach() { announceValue ->
-                    var ticketMatrix = iterateThrough(ticket, announceValue)
-                    if (ticketMatrix[ticketMatrix.size - 2].size == 0) {
-                        if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
-                            return true
-                    }
-                }
-                return false
-            }
-            "Full House" -> {
-                var count = 0
-                announcedNumbers.forEach() { announceValue ->
-                    count = count + iterate(ticket, announceValue)
-                    if (count == 15) {
-                        if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
-                            return true
-                    }
-                }
-                return false
-            }
-            "Early 5" -> {
-                var count = 0
-                announcedNumbers.forEach() { announceValue ->
-                    count = count + iterate(ticket, announceValue)
-                    if (count == 5) {
-                        if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
-                            return true
-                    }
-                }
-                return false
-            }
-            else -> return false
+            return false
         }
+        else if(claim == "Bottom Row") {
+            announcedNumbers.forEach() { announceValue ->
+                var ticketMatrix = iterateThrough(ticket, announceValue)
+                if (ticketMatrix[ticketMatrix.size-1].size == 0) {
+                    if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
+                        return true
+                    else
+                        return false
+                }
+            }
+            return false
+        }
+        else if(claim == "Middle Row") {
+            announcedNumbers.forEach() { announceValue ->
+                var ticketMatrix = iterateThrough(ticket, announceValue)
+                if (ticketMatrix[ticketMatrix.size-2].size == 0) {
+                    if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
+                        return true
+                    else
+                        return false
+                }
+            }
+            return false
+        }
+        else if(claim == "Full House") {
+            var count = 0
+            announcedNumbers.forEach() {announceValue->
+                count = count + iterate(ticket, announceValue)
+                if(count == 15)
+                {
+                    if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
+                        return true
+                    else
+                        return false
+                }
+            }
+            return false
+        }
+        else if(claim == "Early 5") {
+            var count = 0
+            announcedNumbers.forEach() {announceValue->
+                count = count + iterate(ticket, announceValue)
+                if(count == 5)
+                {
+                    if (announcedNumbers.indexOf(announceValue) == announcedNumbers.size - 1)
+                        return true
+                    else
+                        return false
+                }
+            }
+            return false
+        }
+        else
+            return false
     }
-
-    private fun iterateThrough(ticket: Ticket, announceValue: Int): MutableList<MutableList<Int>> {
+    fun iterateThrough(ticket: Ticket, announceValue: Int): MutableList<MutableList<Int>>{
         val ticketMatrix = ticket.ticketVal
         val numberOfRows = ticket.ticketVal.size
-        for (rowNumber in 0..<numberOfRows) {
+        for (rowNumber in 0..<numberOfRows)
+        {
             var numberOfColumns = ticketMatrix[rowNumber].size
-            for (columnNumber in 0..<numberOfColumns) {
-                if (ticketMatrix[rowNumber][columnNumber] == announceValue) {
+            for(columnNumber in 0..<numberOfColumns)
+            {
+                if(ticketMatrix[rowNumber][columnNumber] == announceValue)
+                {
                     ticketMatrix[rowNumber].remove(announceValue)
-                    return ticketMatrix
+                    return  ticketMatrix
+
                 }
+
             }
         }
         return ticketMatrix
     }
-
-    private fun iterate(ticket: Ticket, announceValue: Int): Int {
+    fun iterate(ticket: Ticket, announceValue: Int): Int{
         val ticketMatrix = ticket.ticketVal
         val numberOfRows = ticket.ticketVal.size
-        for (rowNumber in 0..<numberOfRows) {
+        for (rowNumber in 0..<numberOfRows)
+        {
             var numberOfColumns = ticketMatrix[rowNumber].size
-            for (columnNumber in 0..<numberOfColumns) {
-                if (ticketMatrix[rowNumber][columnNumber] == announceValue) {
+            for(columnNumber in 0..<numberOfColumns)
+            {
+                if(ticketMatrix[rowNumber][columnNumber] == announceValue)
+                {
                     ticketMatrix[rowNumber].remove(announceValue)
-                    return 1
+                    return  1
+
                 }
+
             }
         }
         return 0
