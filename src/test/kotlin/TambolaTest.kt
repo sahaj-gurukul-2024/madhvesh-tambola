9e4dfd3bdbdd280271f9d30be0ab6d580695f079
Siddhant Jagdish Verma <siddhantjagdishv@Siddhants-MacBook-Pro.local>
2024-02-15 16:43:43 +0530

    Created Initial Test Case

--- src/test/kotlin/TambolaTest.kt
+++ src/test/kotlin/TambolaTest.kt
@@ -11,6 +11,38 @@ class TambolaTest {
         game = Game()
     }
     @Test
+    fun `check for Top Row claim as True in ticket`(){
+        //arrange
+        val ticketValues = mutableListOf(
+            mutableListOf(4, 16, 48, 63, 76),
+            mutableListOf(7, 23, 38, 52, 80),
+            mutableListOf(9, 25, 56, 64, 83)
+        )
+        val announcedNumbers = listOf(4, 23, 38, 48, 63, 52, 76)
+        val claim = "Top Row"
+        //act
+        val ticket = Ticket(ticketValues)
+        val booleanValue = game.validate(ticket, announcedNumbers, claim)
+        //
+        assertTrue(booleanValue)
+    }
+    @Test
+    fun `check for Early 5 row claim as True in ticket`(){
+        //arrange
+        val ticketValues = mutableListOf(
+            mutableListOf(4, 16, 48, 63, 76),
+            mutableListOf(7, 23, 38, 52, 80),
+            mutableListOf(9, 25, 56, 64, 83)
+        )
+        val announcedNumbers = listOf(4, 23, 48, 63, 76)
+        val claim = "Early 5"
+        //act
+        val ticket = Ticket(ticketValues)
+        val booleanValue = game.validate(ticket, announcedNumbers, claim)
+        //
+        assertTrue(booleanValue)
+    }
+    @Test
     fun `check for Early 5 row claim as False in ticket`(){
         //arrange
         val ticketValues = mutableListOf(
@@ -26,4 +58,5 @@ class TambolaTest {
         //
         assertFalse(booleanValue)
     }
+
 }
